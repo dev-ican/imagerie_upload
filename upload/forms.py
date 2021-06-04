1d2ace89cae6e8634e9af7a5af8b9ba16b7f5069
ZasshuNeko <nicolas.mazaleyrat@gmail.com>
2021-06-04 11:48:23 +0200

    Ajout du datePicker pour le formulaire upload

--- upload/forms.py
+++ upload/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.core.validators import *
 from django.http import *
 from datetime import date
+from bootstrap_datepicker_plus import DatePickerInput
 
 from .models import *
     
@@ -17,7 +18,7 @@ class UploadForm(forms.Form):
     ''' Formulaire de chargement de fichier '''
     nip = forms.CharField(label='Numéro patient', max_length=100)
     etudes = forms.ChoiceField(widget=forms.Select(), choices=(['liste de vos études']), initial='0', required=True,)
-    date_irm = forms.DateField()
+    date_irm = forms.DateField(label="Date de l'examen",widget=DatePickerInput(format='%Y-%m-%d'), required=True)
     upload = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
 
