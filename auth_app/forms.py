5a1cd3dc275d21bfcebba1344117d5ffe7e004c3
Alexandre JURQUET <a.jurquet@ihuican.org>
2022-12-08 16:45:28 +0100

    t

--- auth_app/forms.py
+++ auth_app/forms.py
@@ -8,8 +8,8 @@ from .models import *
 class LogIn(forms.Form):
     ''' Formulaire de connexion à l'application '''
     log_id = forms.CharField(label="Identifiant", max_length=100)
-    pwd = forms.CharField(
-        label="Mot de passe",
-        widget=forms.PasswordInput(),
-        max_length=100,
-    )
+    pwd = forms.CharField(label="Mot de passe",
+                          widget=forms.PasswordInput(),
+                          max_length=100,
+                          )
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox())
